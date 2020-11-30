cdd7843cb96b6b17cb3861b5e897cf215b46046d
jsusilo <enr@bk.ru>
2020-12-01 00:13:57 +0500

    about page

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "../components/About/Header";
+import Content from "../components/About/Content";
+import Footer from "../components/About/Footer";
+
+const About = () => {
+  return (
+    <div className="page">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+};
+
+export default About;
