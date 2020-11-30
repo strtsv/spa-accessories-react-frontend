5f038665e121c14a86c94235dfb8ae0e551b7c85
jsusilo <enr@bk.ru>
2020-11-30 21:14:52 +0500

    deps

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import "./assets/css/grid.css";
+import "./assets/css/style.css";
+import "./assets/css/subscribeform.css";
+import "./assets/css/camera.css";
+import "./assets/css/font-awesome.min.css";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import News from "./pages/News";
+import Contacts from "./pages/Contacts";
+import Privacy from "./pages/Privacy";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/index-1.html" component={About} />
+      <Route exact path="/index-2.html" component={Contacts} />
+      <Route exact path="/index-3.html" component={News} />
+      <Route exact path="/index-4.html" component={Products} />
+      <Route exact path="/index-5.html" component={Privacy} />
+      <Route exact path="/" component={Home} />
+    </Switch>
   );
 }
 
