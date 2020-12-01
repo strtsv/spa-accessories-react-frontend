19795900a89b9863d109d687c2c969e8382743cb
jsusilo <enr@bk.ru>
2020-12-02 00:02:50 +0500

    news page

---
+++ src/pages/News.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "../components/News/Header";
+import Content from "../components/News/Content";
+import Footer from "../components/News/Footer";
+
+const News = () => {
+  return (
+    <div className="page">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+};
+
+export default News;
