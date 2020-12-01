736210058f064bec3295004a7991247318f6efbb
jsusilo <enr@bk.ru>
2020-12-02 00:02:33 +0500

    contacts page

---
+++ src/pages/Contacts.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "../components/Contacts/Header";
+import Content from "../components/Contacts/Content";
+import Footer from "../components/Contacts/Footer";
+
+const Contacts = () => {
+  return (
+    <div className="page">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+};
+
+export default Contacts;
