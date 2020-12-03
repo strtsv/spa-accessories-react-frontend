6b06b0e372c5a8fc0dc929ed1d9c0b168e5f56fe
jsusilo <enr@bk.ru>
2020-12-03 23:36:04 +0500

    products page

---
+++ src/pages/Products.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "../components/Products/Header";
+import Content from "../components/Products/Content";
+import Footer from "../components/Products/Footer";
+
+const Products = () => {
+  return (
+    <div className="page">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+};
+
+export default Products;
