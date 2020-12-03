da884984c5dc9cbe909486aaa70312ceca4f3817
jsusilo <enr@bk.ru>
2020-12-04 00:03:31 +0500

    comp contacts content

---
+++ src/components/Contacts/Content.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Contact from "./Contact";
+import Addresses from "./Addresses";
+
+class Content extends React.Component {
+  render() {
+    return (
+      <main>
+        {}
+        <Contact></Contact>
+        {}
+        {}
+        <Addresses></Addresses>
+        {}
+      </main>
+    );
+  }
+}
+
+export default Content;
