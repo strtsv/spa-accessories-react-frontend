96e2e56100232d7042f211a97ab017e7d9d3beb6
jsusilo <enr@bk.ru>
2020-12-09 00:50:42 +0500

    news cpntent comp

---
+++ src/components/News/Content.js
@@ -0,0 +1,20 @@
+import React from "react";
+import News from "./News";
+import Videos from "./Videos";
+
+class Content extends React.Component {
+  render() {
+    return (
+      <main>
+        {}
+        <News></News>
+        {}
+        {}
+        <Videos></Videos>
+        {}
+      </main>
+    );
+  }
+}
+
+export default Content;
