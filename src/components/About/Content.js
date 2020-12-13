426f3e653b4a48617723a0cd91affebed99739b5
jsusilo <enr@bk.ru>
2020-12-13 12:31:44 +0500

    about content

---
+++ src/components/About/Content.js
@@ -0,0 +1,24 @@
+import React from "react";
+import AboutUs from "./AboutUs";
+import OurStaff from "./OurStaff";
+import OurStandards from "./OurStandards";
+
+class Content extends React.Component {
+  render() {
+    return (
+      <main>
+        {}
+        <AboutUs></AboutUs>
+        {}
+        {}
+        <OurStaff></OurStaff>
+        {}
+        {}
+        <OurStandards></OurStandards>
+        {}
+      </main>
+    );
+  }
+}
+
+export default Content;
