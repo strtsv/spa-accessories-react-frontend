753e9a71cf4cc1fb94021fccdfce27c7f3903fc7
jsusilo <enr@bk.ru>
2020-12-17 00:24:28 +0500

    products content

---
+++ src/components/Products/Content.js
@@ -0,0 +1,24 @@
+import React from "react";
+import SwimSpas from "./SwimSpas";
+import FeaturedArea from "./FeaturedArea";
+import Parallax from "./Parallax";
+
+class Content extends React.Component {
+  render() {
+    return (
+      <main>
+        {}
+        <SwimSpas></SwimSpas>
+        {}
+        {}
+        <FeaturedArea></FeaturedArea>
+        {}
+        {}
+        <Parallax></Parallax>
+        {}
+      </main>
+    );
+  }
+}
+
+export default Content;
