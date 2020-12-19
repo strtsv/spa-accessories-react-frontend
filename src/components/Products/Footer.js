7cf7421e1dfeb7ac7ad00955b670a561d948f04a
jsusilo <enr@bk.ru>
2020-12-19 18:35:28 +0500

    products footer

---
+++ src/components/Products/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import i from "../../assets/images/logo-footer.png";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="well-md">
+        <div className="container">
+          <a href="./">
+            <img src={i} alt />
+          </a>
+          <p className="copyright">
+            Copyright © <span id="copyright-year" /> |
+            <a href="index-5.html">Privacy Policy</a>
+          </p>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
